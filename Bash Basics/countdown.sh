15dc5da5dfdf20bf8c857a867d7e4c22d0420db0
devopsplay-code <sribalajidevops@gmail.com>
2025-09-29 04:43:41 +0000

    Bash Basic Script

---
+++ Bash Basics/countdown.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+for (( i=5; i>=1; i-- ))
+do
+    echo "Countdown: $i"
+    sleep 1   # pause for 1 second
+done
+
+echo "🚀 Lift off!"
---
+++ Bash Basics/greet.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+read -p "Enter your name: " name
+echo "Welcome, $name! 👋"
